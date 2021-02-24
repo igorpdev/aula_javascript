ad55d6897397dc460e1719d7023834e07f3f2262
Igor Pereira <igorjps@uol.com.br>
2021-02-23 22:56:46 -0300

    parte 2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,51 @@
-var nome = "Igor Pereira";
+/*var d = new Date();
+alert(d.getMonth());            //biblioteca data
+alert(d.getUTCHours());
+alert(d.getUTCMinutes());*/
+
+
+/*var count;
+for (count = 0; count <= 5; count ++) {   //contador com para
+    alert(count);
+}*/
+
+
+/*var count = 0;
+while (count <= 5) {       //contador com enquanto
+    console.log(count);
+    count ++;
+}*/
+
+
+/*var idade = prompt("Qual sua idade?") //o usuário insere o valor
+if (idade >= 18) {
+    alert("maior de idade");
+} else{
+    alert("menor de idade")
+}*/
+
+
+/*var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxo"}] //lista de dicionários
+console.log(frutas[1]);
+alert(frutas[0].nome);
+*/
+
+/*var fruta = {nome:"maçã", cor:"vermelha"} //dicionário
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["Maçã", "Pêra", "Laranja"]
+//lista.push("Uva"); adiciona a array
+//lista.pop(); remove o ultimo elemento da array
+//console.log(lista[1]); printa o elemento correspondente
+//console.log(lista.length); printa o tamanho da lista
+//console.log(lista.reverse()); reverte a ordem da array
+//console.log(lista.toString()) transforma a array em string
+//console.log(lista.join(" - ")) transforma em string e adiciona o que quer
+
+
+/*var nome = "Igor Pereira";
 var idade = 24
 alert(nome + " tem " + idade + " anos");
+*/
